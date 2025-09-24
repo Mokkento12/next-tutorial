85c7d018db8d1b8bea0c7d281a35bd312077f6ad
roman alekseev <roma.alekseev.90@bk.ru>
2025-09-24 09:23:08 +0300

    Добавлены алиасы для компонентов+подключен новый шрифт

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,12 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import Footer from '@/components/Footer';
 import Navigation from '@/components/Navigation';
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
+export const inter = Inter({
+	subsets: ['cyrillic', 'latin'],
+	display: 'swap',
+	variable: '--font-inter',
 });
 
 export const metadata = {
@@ -20,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={inter.variable}>
 			<body>
 				<div className="bg-grid"></div>
 				<Navigation />
