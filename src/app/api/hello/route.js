5f615688b8465d07e0d54b615b5aeb3f321e8a5d
roman alekseev <roma.alekseev.90@bk.ru>
2025-10-01 08:49:00 +0300

    Исправление антипаттерна

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,13 +1,13 @@
 import { NextResponse } from 'next/server';
+import { getHelloMessage, createHelloGreeting } from '@/lib/hello';
 
 export async function GET() {
-	return NextResponse.json({ message: 'Hello, from API!' });
+	const data = getHelloMessage();
+	return NextResponse.json(data);
 }
 
 export async function POST(req) {
-	const data = await req.json();
-	const { name } = data;
-	return NextResponse.json({
-		message: `Hello ${name}, from API post request!`,
-	});
+	const { name } = await req.json();
+	const greeting = createHelloGreeting(name);
+	return NextResponse.json(greeting);
 }
